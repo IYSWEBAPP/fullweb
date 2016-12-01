cc28a9e2ca349976cf73d12cf06defaff50f4d4d
joseluismq <mqjoseluis@gmail.com>
2016-12-01 10:34:40 -0300

    install laravel

---
+++ dev/iys/app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace buscotrabajo\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'buscotrabajo\Events\SomeEvent' => [
+            'buscotrabajo\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}
